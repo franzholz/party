3955f971e4dd237b2b14ec06b45d29b22dd5a735
dbruehlmeier <dbruehlmeier@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-01-16 16:43:31 +0000

    Initial commit
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/party/trunk@4626 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "party"
+#
+# Auto generated 16-01-2007 17:40
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Party Information Framework',
+	'description' => 'Management of party-related data, such as persons, organisations, names, addresses, etc. Based on the OASIS-Standard xPIL (Extensible Party Information Language).',
+	'category' => 'be',
+	'author' => 'David Bruehlmeier',
+	'author_email' => 'typo3@bruehlmeier.com',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"b253";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"27ae";s:20:"doc/wizard_form.html";s:4:"3f56";}',
+);
+
+?>
