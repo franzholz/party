ca3a23ad16a1ab4dd6a47e3bab73bceb9eb6c6f3
Franz Holzinger <franz.holzinger@festwein.de>
2024-01-14 10:33:14 +0100

    Deprecation: #85836 - BackendUtility::getTCAtypes

--- Classes/Api/Api.php
+++ Classes/Api/Api.php
@@ -1,35 +1,23 @@
 <?php
-/***************************************************************
-*  Copyright notice
-*
-*  (c) 2013 Franz Holzinger (franz@ttproducts.de)
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license
-*  from the author is found in LICENSE.txt distributed with these scripts.
-*
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+
+namespace JambageCom\Party\Api;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
 /**
  * Part of the party (Party Management Framework) extension.
  *
  * API functions
- *
- * $Id$
  *
  * @author  Franz Holzinger <franz@ttproducts.de>
  * @maintainer	Franz Holzinger <franz@ttproducts.de>
@@ -39,7 +27,9 @@
  */
 
 
-class tx_party_api {
+class Api {
+	static const $baseFields = 'uid,pid,tstamp,crdate,deleted,';
+	
 	static private $tableArray = array(
 		'tx_party_accounts',
 		'tx_party_address_usages',
