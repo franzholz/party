30cff4e7386c1638d6c6bc0495e422c336a747f4
Franz Holzinger <franz.holzinger@festwein.de>
2024-01-17 14:57:57 +0100

    Remove hotlist feature

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,14 +5,7 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 defined('TYPO3') || die('Access denied.');
 
 call_user_func(function ($extensionKey): void {
-    if (!defined('PARTY_EXT')) {
-        define('PARTY_EXT', 'party');
-    }
-
-    // Activate Hooks in TCE-Main for the hotlist updates of static_info_tables
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:party/hooks/class.tx_party_staticinfotables.php:tx_party_staticinfotables';
-
-    require_once(ExtensionManagementUtility::extPath('party') . 'Classes/Api/Api.php');
+    require_once(ExtensionManagementUtility::extPath($extensionKey) . 'Classes/Api/Api.php');
 
     $tableArray = \JambageCom\Party\Api\Api::getTableArray();
 
@@ -23,5 +16,4 @@ call_user_func(function ($extensionKey): void {
             ');
         }
     }
-
 }, 'party');
