c91763b1c714953deab24b2091acb471d1fa25aa
Franz Holzinger <franz.holzinger@festwein.de>
2024-01-17 14:30:59 +0100

    use anonymous function

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,22 +4,24 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') || die('Access denied.');
 
-if (!defined('PARTY_EXT')) {
-    define('PARTY_EXT', 'party');
-}
-
-// Activate Hooks in TCE-Main for the hotlist updates of static_info_tables
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:party/hooks/class.tx_party_staticinfotables.php:tx_party_staticinfotables';
-
-require_once(ExtensionManagementUtility::extPath('party') . 'Classes/Api/Api.php');
-
-$tableArray = \JambageCom\Party\Api\Api::getTableArray();
-
-if (isset($tableArray) && is_array($tableArray)) {
-    foreach ($tableArray as $tablename) {
-        ExtensionManagementUtility::addUserTSConfig('
-			options.saveDocNew.' . $tablename . '=1
-		');
+call_user_func(function ($extensionKey): void {
+    if (!defined('PARTY_EXT')) {
+        define('PARTY_EXT', 'party');
     }
-}
 
+    // Activate Hooks in TCE-Main for the hotlist updates of static_info_tables
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:party/hooks/class.tx_party_staticinfotables.php:tx_party_staticinfotables';
+
+    require_once(ExtensionManagementUtility::extPath('party') . 'Classes/Api/Api.php');
+
+    $tableArray = \JambageCom\Party\Api\Api::getTableArray();
+
+    if (isset($tableArray) && is_array($tableArray)) {
+        foreach ($tableArray as $tablename) {
+            ExtensionManagementUtility::addUserTSConfig('
+                options.saveDocNew.' . $tablename . '=1
+            ');
+        }
+    }
+
+}, 'party');
