03af06b3859e83ed1bd355080101aa51197e6f50
Franz Holzinger <franz.holzinger@festwein.de>
2024-01-17 15:13:39 +0100

    use allowedNewTables TSConfig in the Configuration

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,19 +1,4 @@
 <?php
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
 defined('TYPO3') || die('Access denied.');
 
-call_user_func(function ($extensionKey): void {
-    require_once(ExtensionManagementUtility::extPath($extensionKey) . 'Classes/Api/Api.php');
-
-    $tableArray = \JambageCom\Party\Api\Api::getTableArray();
-
-    if (isset($tableArray) && is_array($tableArray)) {
-        foreach ($tableArray as $tablename) {
-            ExtensionManagementUtility::addUserTSConfig('
-                options.saveDocNew.' . $tablename . '=1
-            ');
-        }
-    }
-}, 'party');
